63366bff15b66170cf6aee830af2a9eb179f4344
Peter Pilgrim <peter.pilgrim@gmail.com>
2023-04-26 18:15:50 +0100

    Add parameterised Scala tests. Thanks to these URLs:
    https://stackoverflow.com/questions/65378298/each-case-as-separated-test-in-scalatest-junit-parameterizedtest-style
    
    https://stackoverflow.com/questions/5369243/testing-multiple-data-sets-with-scalatest

--- src/test/scala/uk/co/xenonique/rover/RoverTest.scala
+++ src/test/scala/uk/co/xenonique/rover/RoverTest.scala
@@ -3,10 +3,21 @@ package uk.co.xenonique.rover
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers._
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.prop.TableDrivenPropertyChecks._
 
-class RoverTests extends AnyFunSuite with BeforeAndAfterEach {
+class RoverTests extends AnyFunSuite with BeforeAndAfterEach with TableDrivenPropertyChecks {
 
   var rover: Rover = _
+
+  val ROTATE_RIGHT_COMMANDS = Table(
+    ("R", "E"),
+    ("RR", "S"),
+    ("RRR", "W"),
+    ("RRRR", "N"),
+    ("RRRRR", "E"),
+  )
+
 
   override def beforeEach() {
     rover = new Rover();
@@ -20,4 +31,14 @@ class RoverTests extends AnyFunSuite with BeforeAndAfterEach {
   test("move the rover again") {
     rover.execute("RR") should be("S")
   }
+
+  forAll(ROTATE_RIGHT_COMMANDS) {
+    (commands: String, position: String) =>
+      test( s"move rover with input: [${commands}] should result in final position: [$position]") {
+        rover.execute(commands) should be (position)
+      }
+
+  }
+
 }
+
